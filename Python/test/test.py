db87eb85b46d1645408c2b69fb65e70c8e1a85d8
sanyagribanov <darkmoonlght324@gmail.com>
2022-12-06 13:23:13 +0300

    qwerty

---
+++ Python/test/test.py
@@ -0,0 +1,12 @@
+# будет работать одноименный файл в папке csv, здесь просто код теста
+
+import pytest
+from csv_csv import show_csv,avg_age
+
+def test_wrong_age():
+    with pytest.raises(ZeroDivisionError):
+        assert avg_age()
+
+def show_error():
+    with pytest.raises(FileNotFoundError):
+        show_csv()
