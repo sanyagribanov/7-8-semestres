4c5813ac83b1aff089952154914ba51a30d03c07
palmistriya <86486142+sanyagribanov@users.noreply.github.com>
2023-01-16 14:57:43 +0300

    Add files via upload

---
+++ Demoekzamen/ConsoleApp/SF2022User02Lib/Calculations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User02Lib
+{
+    public class Calculations
+    {
+        public static string[] AvailablePeriods(TimeSpan[] startTimes,
+            int[] durations,TimeSpan beginWorkingTime,
+            TimeSpan endWorkingTime, int consultationTime)
+        {
+            Console.WriteLine("All right");
+            string[] result = new string[] { "test" };
+            return result;
+
+        }
+    }
+}
